6382f21b2219ece83015dfdd07d2d3a28fe4a12e
JAAR <joe.aaron.acosta@outlook.com>
2024-10-10 23:55:28 -0700

    Project prompt and test environment for a Software Sales Class

---
+++ src/SoftwareSales.java
@@ -0,0 +1,35 @@
+/* Programmer: JAAR
+*  Assignment Chapter: 4 assignment 4
+*  Date modified: 10/10/2024
+*  IDE/Tool used: IntelliJ
+ */
+
+// Software Sales
+// A software company sells a package that retails for $99.
+// Quantity discounts are given according to the following table:
+// Discount Table:
+//  10 – 19             20%
+//  20 – 49             30%
+//  50 – 99             40%
+//  100 or more         50%
+// Design a class that stores the number of units sold, and has a method that
+// returns the total cost of the purchase.
+
+// Creates an order for software sales and tracks information for the order.
+public class SoftwareSales {
+    // Units Sold
+
+
+    // constructor
+
+    // Getters?
+        // total units sold
+        // the final cost after the units sold.
+
+    // Setters?
+        // update the number of units sold.
+
+
+
+
+}

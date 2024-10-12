fac66c0670c7ad13226292b34239d140cc5dd2ce
JAAR <joe.aaron.acosta@outlook.com>
2024-10-11 21:51:16 -0700

    Completed class and completed Test Environment for the assignment class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+/* Programmer: JAAR
+*  Assignment Chapter: 4 assignment 4
+*  Date modified: 10/10/2024
+*  IDE/Tool used: IntelliJ
+*/
+
+import java.util.Scanner;
+
+// Represents a test environment for the SoftwareSales Class
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many licenses of our software would you like" +
+                " to purchase?: ");
+        int orderQuantity = input.nextInt(); // Assume the user enters a
+        // valid number.
+        input.nextLine(); // clears the newline char.
+
+        System.out.printf("You inputted %d are you sure? (yes/no): ",
+                orderQuantity);
+        String response = input.nextLine(); // Assume the user enters either
+        // yes or no.
+        if (!response.equalsIgnoreCase("yes")) {
+            System.out.print("How many licenses would you like?: ");
+            orderQuantity = input.nextInt();
+            input.nextLine();
+        }
+
+        SoftwareSales order = new SoftwareSales(orderQuantity);
+        System.out.printf("Your total is %.2f. You saved %.0f%%",
+                order.finalPrice(), order.discountRate() * 100);
     }
 }
--- src/SoftwareSales.java
+++ src/SoftwareSales.java
@@ -2,34 +2,85 @@
 *  Assignment Chapter: 4 assignment 4
 *  Date modified: 10/10/2024
 *  IDE/Tool used: IntelliJ
- */
-
-// Software Sales
-// A software company sells a package that retails for $99.
-// Quantity discounts are given according to the following table:
-// Discount Table:
-//  10 – 19             20%
-//  20 – 49             30%
-//  50 – 99             40%
-//  100 or more         50%
-// Design a class that stores the number of units sold, and has a method that
-// returns the total cost of the purchase.
+*/
 
 // Creates an order for software sales and tracks information for the order.
 public class SoftwareSales {
     // Units Sold
-
-
-    // constructor
-
-    // Getters?
-        // total units sold
-        // the final cost after the units sold.
-
-    // Setters?
-        // update the number of units sold.
+    private int quantitySold;
+    private static final int PRICE = 99;
 
 
 
+    // constructor:
 
+    public SoftwareSales() {
+        quantitySold = 0;
+    }
+
+    public SoftwareSales(int quantitySold) {
+        this.quantitySold = quantitySold;
+    }
+
+    // Setter:
+
+    // Sets the total items sold in an order.
+    // Param:
+    //  - int quantitySold: the number that represents the total items in the
+    //  order.
+    public void setQuantitySold(int quantitySold) {
+        this.quantitySold = quantitySold;
+    }
+
+    // Updates the quantity total in an order.
+    // Param:
+    //  - int quantityChange: the number of units added or subtracted from
+    //  the order.
+    public void updateQuantitySold(int quantityChange) {
+        quantitySold = quantityChange;
+    }
+
+    // Getters:
+
+    // Retrieves the number of units associated with an order.
+    // Return:
+    //  - int quantitySold: current units associated with the order.
+    public int totalUnitsSold() {
+        return quantitySold;
+    }
+
+    // Calculate the final price for the order.
+    // Return:
+    //  - double orderSubtotal
+    public double finalPrice() {
+        return quantitySold * (1 - discountRate()) * PRICE;
+    }
+
+    // Calculates the order discount based on the number of software licenses
+    // sold.
+    // Param:
+    //  - quantitySold
+    // Return:
+    //  - double discount: the discount rate for the order represented as a
+    //      decimal.
+    // Discount Table:
+    //  10 – 19             20%
+    //  20 – 49             30%
+    //  50 – 99             40%
+    //  100 or more         50%
+    public double discountRate() {
+        double discount;
+        if (100 <= quantitySold) {
+            discount = .50;
+        } else if (50 <= quantitySold) {
+            discount = .40;
+        } else if (20 <= quantitySold) {
+            discount = .30;
+        } else if (10 <= quantitySold) {
+            discount = .20;
+        } else {
+            discount = 0;
+        }
+        return discount;
+    }
 }
